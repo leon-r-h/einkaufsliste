3c0a0bfc08013b9cd2f3f822babb06a3288dce8d
z0053f2j <z0053f2j@149.246.8.155>
2025-12-04 09:02:55 +0100

    Updated to JUnit 5.

---
+++ src/main/java/com/siemens/einkaufsliste/Main.java
@@ -0,0 +1,13 @@
+package com.siemens.einkaufsliste;
+
+public final class Main {
+
+	private Main() {
+		System.out.println("Hello World!");
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
---
+++ src/test/java/com/siemens/einkaufsliste/MainTest.java
@@ -0,0 +1,15 @@
+package com.siemens.einkaufsliste;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    @DisplayName("Sanity Check")
+    void shouldAlwaysBeTrue() {
+        assertTrue(true);
+    }
+}
