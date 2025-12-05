a68702141a5d08035a01c6e859dd4a7c2fd63d0b
leon-r-h <leo-maus@ro.ru>
2025-12-05 08:36:10 +0100

    Added gender studies support.

--- src/main/java/com/siemens/einkaufsliste/database/model/User.java
+++ src/main/java/com/siemens/einkaufsliste/database/model/User.java
@@ -1,6 +1,12 @@
 package com.siemens.einkaufsliste.database.model;
 
-public record User(int userID, String firstName, String lastName, int birthDate, String email, String password,
+public record User(int userID, String firstName, String lastName, int birthDate, Gender gender, String email, String password,
 		boolean newsLetter) {
+	
+	public enum Gender {
+		MALE,
+		FEMALE,
+		OTHER
+	}
 
 }
