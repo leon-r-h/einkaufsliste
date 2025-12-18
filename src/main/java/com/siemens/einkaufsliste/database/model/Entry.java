d8ef98b30b84ff7557b725e87a599bcb28d62f5a
leon-r-h <leo-maus@ro.ru>
2025-12-18 14:31:24 +0100

    Removed fancy constructors.

--- src/main/java/com/siemens/einkaufsliste/database/model/Entry.java
+++ src/main/java/com/siemens/einkaufsliste/database/model/Entry.java
@@ -3,9 +3,5 @@ package com.siemens.einkaufsliste.database.model;
 import java.time.LocalDate;
 
 public record Entry(int entryID, int userID, int productID, int quantity, LocalDate checkDate) {
-	
-	public Entry(int userID, int productID, int quantity, LocalDate checkDate) {
-		this(0, userID, productID, quantity, checkDate);
-	}
 
 }
--- src/main/java/com/siemens/einkaufsliste/database/model/Product.java
+++ src/main/java/com/siemens/einkaufsliste/database/model/Product.java
@@ -1,10 +1,6 @@
 package com.siemens.einkaufsliste.database.model;
 
 public record Product(int productID, String name, Category category, String brand, int price) {
-
-	public Product(String name, Category category, String brand, int price) {
-		this(0, name, category, brand, price);
-	}
 	
 	public enum Category {
 		WHEAT, ELECTRONICS, FRUITS, VEGETABLES, MILK, DRINKS, ALCOHOL, SNACKS, HOUSEHOLD
